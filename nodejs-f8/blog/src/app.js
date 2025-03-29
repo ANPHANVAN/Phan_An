dd5d99c6f4f5cd017ae7fc0112f98adb5494956d
An Phan <2k1.xam@gmail.com>
2025-03-29 23:27:16 +0700

    add new project with express

--- nodejs-f8/blog/src/app.js
+++ nodejs-f8/blog/src/app.js
@@ -2,21 +2,18 @@ const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const path = require("path");
-const db = require('./config/db')
-var methodOverride = require('method-override')
+const methodOverride = require('method-override')
+const db = require('./config/db');
 const route = require(path.join(__dirname, "routes"));
-
 const app = express();
 const port = 3000;
 
+// config connect mongoDB
 db.connect()
 
 app.use(methodOverride('_method'))
 app.use(methodOverride('X-HTTP-Method-Override'));
-
 app.use(express.static(path.join(__dirname, "public")));
-// config connect DB
-
 app.use(
   express.urlencoded({
     extended: true,
@@ -25,16 +22,13 @@ app.use(
 app.use(express.json());
 app.use(morgan("combined"));
 
-
 app.engine("hbs", handlebars.engine({ extname: ".hbs",
   helpers: { sum: (a,b) => a+b}
 }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resource/views"));
 
-
 route(app);
-
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
