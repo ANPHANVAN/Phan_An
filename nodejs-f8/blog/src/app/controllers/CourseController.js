6d61d34977c4b6a104ebddf6d614af32acd394f9
An Phan <2k1.xam@gmail.com>
2025-03-29 18:28:37 +0700

    update feature 29/3

--- nodejs-f8/blog/src/app/controllers/CourseController.js
+++ nodejs-f8/blog/src/app/controllers/CourseController.js
@@ -1,38 +1,38 @@
-const Course = require('../models/Courses')
-const { multipleMongooseToObject } = require('../../tool/toobject')
+const Course = require("../models/Courses");
+const { multipleMongooseToObject } = require("../../tool/toobject");
 
 class CourseController {
   async index(req, res) {
     try {
       let courses = await Course.find({});
-      courses = multipleMongooseToObject(courses)
-      res.render('course',{courses: courses})
-    } 
-    catch (err) {
-        res.status(400).json({ error: 'error!' });
+      courses = multipleMongooseToObject(courses);
+      res.render("course", { courses: courses });
+    } catch (err) {
+      res.status(400).json({ error: "error!" });
     }
   }
 
   async slug(req, res) {
     try {
-        const slugName = req.params.slug;
-        let courses = await Course.find({});
-        courses = multipleMongooseToObject(courses);
-        let course = courses.find((course) => course.slug == slugName);
-        res.render('course-detail', course);
+      const slugName = req.params.slug;
+      let courses = await Course.find({});
+      courses = multipleMongooseToObject(courses);
+      let course = courses.find((course) => course.slug == slugName);
+      res.render("course-detail", course);
     } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Something went wrong");
+      console.error("Error:", error);
+      res.status(500).send("Something went wrong");
     }
   }
   async create(req, res) {
-    res.render('course-create')
+    res.render("course-create");
   }
   async store(req, res) {
     const courseCreate = new Course(req.body);
-    await courseCreate.save()
-      .then(() => res.redirect('/course'))
-      .catch(error => res.send('your course dont create'))
+    await courseCreate
+      .save()
+      .then(() => res.redirect("/course"))
+      .catch((error) => res.send("your course dont create"));
   }
 }
 
--- nodejs-f8/blog/src/app/controllers/Me/MyCourseController.js
+++ nodejs-f8/blog/src/app/controllers/Me/MyCourseController.js
@@ -1,18 +1,20 @@
-const Course = require('../../models/Courses')
-const { multipleMongooseToObject, mongooseToObject } = require('../../../tool/toobject')
+const Course = require("../../models/Courses");
+const {
+  multipleMongooseToObject,
+  mongooseToObject,
+} = require("../../../tool/toobject");
 
 class MeController {
-
   // [GET] /me/my-course
   async indexCourse(req, res, next) {
     try {
       let courses = await Course.findActive();
       courses = multipleMongooseToObject(courses);
-      let numberBin = await Course.countDocuments({deleted: true})
-      console.log(numberBin)
-      res.render('me/me-my-course', {courses, numberBin})
+      let numberBin = await Course.countDocuments({ deleted: true });
+      console.log(numberBin);
+      res.render("me/me-my-course", { courses, numberBin });
     } catch (err) {
-      res.status(400).json({ error: 'error!' });
+      res.status(400).json({ error: "error!" });
     }
   }
 
@@ -20,41 +22,37 @@ class MeController {
   async changeCourse(req, res, next) {
     try {
       let id = req.params.id;
-      let courses = await Course.find({})
-      courses = multipleMongooseToObject(courses)
-      let course = courses.find(course => course._id == id)
-      res.render('./me/change-course', {course})
+      let courses = await Course.find({});
+      courses = multipleMongooseToObject(courses);
+      let course = courses.find((course) => course._id == id);
+      res.render("./me/change-course", { course });
+    } catch (err) {
+      res.status(400).json({ error: "error!" });
     }
-    catch (err) {
-      res.status(400).json({ error: 'error!' });
-    }
-
   }
 
   // [POST][PUT] /me/:id
   async change(req, res, next) {
     try {
       let id = req.params.id;
-      let updateCourse = req.body
-      Course.updateOne({_id: id}, updateCourse)
-        .then(() => res.redirect('/me/my-course'))
-        .catch(next)
+      let updateCourse = req.body;
+      Course.updateOne({ _id: id }, updateCourse)
+        .then(() => res.redirect("/me/my-course"))
+        .catch(next);
+    } catch (err) {
+      res.status(400).json({ error: "error!" });
     }
-    catch (err) {
-      res.status(400).json({ error: 'error!' });
-    }
-
   }
 
   // [DELETE] /me/:id
   async delete(req, res, next) {
     try {
       let id = req.params.id;
-      Course.delete({_id: id})
-        .then(() => res.redirect('/me/my-course'))
-        .catch(next)
+      Course.delete({ _id: id })
+        .then(() => res.redirect("/me/my-course"))
+        .catch(next);
     } catch (err) {
-      res.status(400).json({ error: 'error!' });
+      res.status(400).json({ error: "error!" });
     }
   }
 
@@ -63,48 +61,45 @@ class MeController {
     try {
       let id = req.params.id;
       // res.send(id)
-      Course.restore({_id: id})
-        .then(() => res.redirect('/me/bin'))
-        .catch(next)
-    }
-    catch (err) {
-      res.status(400).json({ error: 'error!' });
+      Course.restore({ _id: id })
+        .then(() => res.redirect("/me/bin"))
+        .catch(next);
+    } catch (err) {
+      res.status(400).json({ error: "error!" });
     }
   }
 
-    // [GET] /me/bin
-    async bin(req, res, next) {
-      try {
-        let courseDeletes = await Course.findDeleted()
-        courseDeletes = multipleMongooseToObject(courseDeletes)
-        res.render('me/bin', {courseDeletes})
-      }
-      catch (err) {
-        res.status(400).json({ error: 'error!' });
-      }
+  // [GET] /me/bin
+  async bin(req, res, next) {
+    try {
+      let courseDeletes = await Course.findDeleted();
+      courseDeletes = multipleMongooseToObject(courseDeletes);
+      res.render("me/bin", { courseDeletes });
+    } catch (err) {
+      res.status(400).json({ error: "error!" });
     }
+  }
 
-      // [DELETE] /me/:id/permanent
+  // [DELETE] /me/:id/permanent
   async destroy(req, res, next) {
     try {
       let id = req.params.id;
       Course.findOneAndDelete({ _id: id })
-        .then(() => res.redirect('/me/bin'))
-        .catch(next)
+        .then(() => res.redirect("/me/bin"))
+        .catch(next);
     } catch (err) {
-      res.status(400).json({ error: 'error!' });
+      res.status(400).json({ error: "error!" });
     }
   }
 
   async deleteAll(req, res, next) {
     try {
-      let courseIds = req.body.courseIds
+      let courseIds = req.body.courseIds;
       Course.deleteMany({ _id: { $in: courseIds } })
-        .then(() => res.redirect('/me/my-course'))
-        .catch(next)
-
+        .then(() => res.redirect("/me/my-course"))
+        .catch(next);
     } catch (err) {
-      res.status(400).json({ error: 'error!' });
+      res.status(400).json({ error: "error!" });
     }
   }
 }
